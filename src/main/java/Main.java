004562d19ff13d8d2c9ce6de79de0d9840d29b66
Memokor <124714378+Chikckchirick321@users.noreply.github.com>
2023-08-24 20:13:30 +0300

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,7 +6,7 @@ public class Main {
         post.patronymic = "Иванович";
         post.phone = "+7 (999)-999-99-99";
         post.passport = "4444 № 44444444";
-        post.subscription = "true";
+        post.subscription = true;
         post.birthday = new FormDate();
         post.birthday.day = 13;
         post.birthday.month = 6;
